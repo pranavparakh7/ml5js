5441df55a623d3974629925e1d2128b43b794773
Pranav Parakh <31867549+pranavparakh7@users.noreply.github.com>
2021-04-04 23:31:01 +0530

    ml5 added

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ let mobilenet
 function setup(){
   createCanvas(400,400)
   video = createCapture(VIDEO)
-  //video.hide()
-  //mobilenet = ml5.imageClassifier();
+  video.hide()
+  mobilenet = ml5.imageClassifier();
 }
 
 function draw(){
-  //image(video,0,0,320,240)
+  image(video,0,0,320,240)
 }
